51ffc620a4812b85bf8bba36218079c352f79664
ojasvigupta30 <ojasvigupta30@gmail.com>
2024-11-24 20:12:26 -0600

    Dungeons Page created

---
+++ src/pages/Dungeons.jsx
@@ -0,0 +1,12 @@
+import DungeonList from '../components/DungeonList';
+
+const Dungeons = () => {
+    return (
+        <div>
+            <h1>Dungeons</h1>
+            <DungeonList />
+        </div>
+    );
+};
+
+export default Dungeons;
