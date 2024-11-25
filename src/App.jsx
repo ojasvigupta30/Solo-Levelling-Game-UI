6f635d4257cd3ea44bc24c50a020d314f82c8b6e
ojasvigupta30 <ojasvigupta30@gmail.com>
2024-11-25 00:18:28 -0600

    Updated App.jsx with dungeon detail link

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Dungeons from './pages/Dungeons';
 import NavigationButtons from './components/NavigationButtons'; // Separate component for buttons
+import DungeonDetail from './components/DungeonDetail';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dungeons" element={<Dungeons />} />
+        <Route path="/dungeons/:id" element={<DungeonDetail />} />
       </Routes>
     </div>
   );
